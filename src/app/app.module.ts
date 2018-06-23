c9b682d061d47c06cce9d59a024b3fadb9b2f414
Sayed-M <sayko102030@gmail.com>
2018-06-23 20:08:46 +0200

    git commit "initiaize app"

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { SearchService } from './services/search.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [SearchService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
